91eaa2d50ff6cce9777843f680739b84c485311e
Christoph Dinh <christoph.dinh@brain-link.de>
2026-01-21 20:00:54 +0100

    windows fixes

--- atlas_loader.py
+++ atlas_loader.py
@@ -4,6 +4,17 @@ import trimesh
 import os
 import subprocess
 import nibabel as nib
+import ssl
+
+# Patch SSL to avoid errors with nilearn/nitrc
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 
 
